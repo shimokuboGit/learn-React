52ab8bcaeaed4ec5c91879a41a56ca13aff184f7
SHIMOKUBO-source <shota1002.1002@icloud.com>
2024-06-14 21:38:36 +0900

    戻す機能

--- learn-syntax/src/index.mjs
+++ learn-syntax/src/index.mjs
@@ -3,7 +3,10 @@ import "./styles.css"
 const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
+  createIncompleteTodo(inputText)
+}
 
+const createIncompleteTodo = (inputText) => {
   const li = document.createElement("li");
   const div = document.createElement("div");
   div.className = "list-row";
@@ -14,12 +17,19 @@ const onClickAdd = () => {
 
   const completeButton = document.createElement("button")
   completeButton.innerText = '完了'
+
   completeButton.addEventListener('click', () => {
     const completeTarget = completeButton.closest('li')
     completeButton.nextElementSibling.remove()
     completeButton.remove()
+
     const backButton = document.createElement('button')
     backButton.innerText = '戻す'
+    backButton.addEventListener('click', () => {
+      backButton.closest('li').remove()
+      const inputText = backButton.previousElementSibling.innerText
+      createIncompleteTodo(inputText)
+    })
     completeTarget.firstElementChild.appendChild(backButton)
 
     document.getElementById('done-list').appendChild(completeTarget)
@@ -29,15 +39,15 @@ const onClickAdd = () => {
   deleteButton.innerText = '削除'
   deleteButton.addEventListener('click', () => {
     const deleteTarget = deleteButton.closest('li')
-    document.getElementById('yet-todo-list').removeChild(deleteTarget)
+    document.getElementById('incomplete-list').removeChild(deleteTarget)
   })
 
   div.appendChild(p);
   div.appendChild(completeButton)
   div.appendChild(deleteButton)
   li.appendChild(div);
-  
-  document.getElementById("yet-todo-list").appendChild(li);
+
+  document.getElementById("incomplete-list").appendChild(li);
 }
 
 document.getElementById("add-button").addEventListener("click", onClickAdd);
