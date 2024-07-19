bc005164efab0e2d5c1d7cf5d4e4dd53c6d2a893
SHIMOKUBO-source <shota1002.1002@icloud.com>
2024-07-20 02:42:58 +0900

    supabaseに登録できる

--- learning-management-app/src/App.tsx
+++ learning-management-app/src/App.tsx
@@ -5,14 +5,31 @@ import './App.css';
 import { InputLearnRecord } from './components/InputLearnRecord';
 import { LearnRecord } from './domain/LearnRecord';
 
+const supabase = createClient<Database>(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY
+)
 export const App = () => {
-  const supabase = createClient<Database>(import.meta.env.SUPABASE_URL, import.meta.env.SUPABASAE_KEY)
+
+  useEffect(() => {
+    const fetchLearnRecord = async () => {
+      const { data, error } = await supabase.from("study-record").select()
+      const result: LearnRecord[] = data?.map((d) => (
+        {title: d.title, time: d.time}
+      )) || []
+      setRecords(result)
+      setLearnRecordIsLoading(false)
+    }
+    fetchLearnRecord()
+  }, [])
 
   const [inputLearnTitle, setInputLearnTitle] = useState('')
   const [inputLearnTime, setInputLearnTime] = useState<number>(0);
   const [records, setRecords] = useState<LearnRecord[]>([])
-  const [error, setError] = useState<boolean>(false)
+  const [isError, setIsError] = useState<boolean>(false)
+  const [learnRecordisLoading, setLearnRecordIsLoading] = useState<boolean>(true)
   const [totalLearnTime, setTotalLearnTime] = useState(0)
+  
 
   const onChangeLearnTitle = (event: ChangeEvent<HTMLInputElement>) => {
     setInputLearnTitle(event.target.value)
@@ -22,14 +39,15 @@ export const App = () => {
     setInputLearnTime(parseInt(event.target.value))
   }
 
-  const onClickRegister = () => {
+  const onClickRegister = async () => {
     if ((inputLearnTitle || inputLearnTime) === '') {
-      setError(true)
+      setIsError(true)
       return
     }
-    setError(false)
+    setIsError(false)
 
     setRecords([...records, { title: inputLearnTitle, time: inputLearnTime}])
+    await supabase.from("study-record").insert({ title: inputLearnTitle, time: inputLearnTime })
     
     setInputLearnTitle('')
     setInputLearnTime(0)
@@ -38,32 +56,36 @@ export const App = () => {
   useEffect(() => {
     const newTotal = records.reduce((total, value) => total + value.time, 0)
     setTotalLearnTime(newTotal)
-  }, [records])  
+  }, [records])
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>学習記録一覧</h1>
-        <InputLearnRecord 
-          inputTitle={inputLearnTitle}
-          inputTime={inputLearnTime}
-          onChangeTitle={onChangeLearnTitle}
-          onChangeTime={onChangeLearnTime} 
-        />
-
-        <button onClick={onClickRegister}>登録</button>
-        {error && (<p style={{color: 'red'}}>入力されていない項目があります</p>)}
-        <ul> 
-        {records.map((record, index) => (
-          <li key={index}>
-            <p>{record.title} {record.time}時間</p>
-          </li>
-        ))}
-        </ul>
-        <div>
-          合計時間: {totalLearnTime}/1000(h)
-        </div>
-      </header>
-    </div>
-  );
+  if (learnRecordisLoading) {
+    return ( <h1>Loading...</h1> )
+  } else {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>学習記録一覧</h1>
+          <InputLearnRecord 
+            inputTitle={inputLearnTitle}
+            inputTime={inputLearnTime}
+            onChangeTitle={onChangeLearnTitle}
+            onChangeTime={onChangeLearnTime} 
+          />
+  
+          <button onClick={onClickRegister}>登録</button>
+          {isError && (<p style={{color: 'red'}}>入力されていない項目があります</p>)}
+          <ul> 
+          {records.map((record, index) => (
+            <li key={index}>
+              <p>{record.title} {record.time}時間</p>
+            </li>
+          ))}
+          </ul>
+          <div>
+            合計時間: {totalLearnTime}/1000(h)
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
---
+++ learning-management-app/src/components/atoms/Button.tsx
@@ -0,0 +1,6 @@
+export const Button = (props: { text: string }) => {
+  const { text } = props
+  return (
+    <button>{text}</button>
+  )
+}
