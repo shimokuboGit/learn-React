8cf982d9eb9d8c490001af961885c3509c320b6c
Shimokubo <shota1002.1002@icloud.com>
2024-09-13 01:17:18 +0900

    pipeline

--- v2_learning-management-app/src/App.tsx
+++ v2_learning-management-app/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>v2_LEARNING_MANAGEMENT_APPP</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
