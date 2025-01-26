15d5778380360367266e45e0f00d50ed7888bf1c
Shimokubo <shota1002.1002@icloud.com>
2025-01-26 17:32:14 +0900

    deploy

--- next-tech-blog/app/page.tsx
+++ next-tech-blog/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-3xl font-bold under-line">
-        Hello World!3
+        Hello World!4
       </h1>
       <button className="btn">Button</button>
       <Title />
